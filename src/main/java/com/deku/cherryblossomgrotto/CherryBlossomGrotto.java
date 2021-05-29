ceb651dc8e27155b36f9de2b75f350d88dc7d995
Dekunutter <jordan.griffin@outlook.ie>
2021-05-29 17:02:18 +0100

    Fix build scripts

--- src/main/java/com/deku/cherryblossomgrotto/CherryBlossomGrotto.java
+++ src/main/java/com/deku/cherryblossomgrotto/CherryBlossomGrotto.java
@@ -72,7 +72,7 @@ public class CherryBlossomGrotto
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("cherryblossomgrotto", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
