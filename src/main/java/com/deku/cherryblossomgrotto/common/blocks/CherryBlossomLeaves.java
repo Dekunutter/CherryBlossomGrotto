6f0e43103c91a27421f34f781111ff20c5d004cf
Dekunutter <jordan.griffin@outlook.ie>
2021-10-10 18:42:37 +0100

    Add flammability to other blocks

--- src/main/java/com/deku/cherryblossomgrotto/common/blocks/CherryBlossomLeaves.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/blocks/CherryBlossomLeaves.java
@@ -9,6 +9,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
@@ -65,5 +66,17 @@ public class CherryBlossomLeaves extends LeavesBlock {
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new CherryLeavesTileEntity();
+    }
+
+    @Override
+    public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face)
+    {
+        return 5;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face)
+    {
+        return 20;
     }
 }
--- src/main/java/com/deku/cherryblossomgrotto/common/blocks/CherryBlossomLog.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/blocks/CherryBlossomLog.java
@@ -17,12 +17,30 @@ public class CherryBlossomLog extends RotatedPillarBlock {
         setRegistryName("cherry_blossom_log");
     }
 
+    /**
+     * Sets the flammability of the block. The higher the number the more likely it is to catch fire
+     *
+     * @param state State of the block
+     * @param world World that the block exists in
+     * @param pos Position of the block
+     * @param face The face of the block being set on fire
+     * @return The flammability value of the block given its position in the world and the face being set alight
+     */
     @Override
     public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face)
     {
         return 5;
     }
 
+    /**
+     * Determines how likely the fire is to destroy the block. The higher the number the more likely it is to burn up.
+     *
+     * @param state State of the block
+     * @param world World that the block exists in
+     * @param pos Position of the block
+     * @param face The face of the block that's currently aflame
+     * @return The likelihood that this burning block will be destroyed by the fire
+     */
     @Override
     public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face)
     {
