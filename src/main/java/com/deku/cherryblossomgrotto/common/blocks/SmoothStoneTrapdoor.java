4e6ff6f7ee7c7fe54a1b20c14ab397168c7e06d3
Dekunutter <jordan.griffin@outlook.ie>
2022-04-03 21:21:17 +0100

    Fix missing import in smooth stone trapdoor

--- src/main/java/com/deku/cherryblossomgrotto/common/blocks/SmoothStoneTrapdoor.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/blocks/SmoothStoneTrapdoor.java
@@ -2,12 +2,13 @@ package com.deku.cherryblossomgrotto.common.blocks;
 
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 
 public class SmoothStoneTrapdoor extends TrapDoorBlock {
     public SmoothStoneTrapdoor() {
-        super(AbstractBlock.Properties.of(Material.STONE, MaterialColor.STONE).strength(2.0f, 6.0f).sound(SoundType.STONE));
+        super(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.STONE).strength(2.0f, 6.0f).sound(SoundType.STONE));
         setRegistryName("smooth_stone_trapdoor");
     }
 }
