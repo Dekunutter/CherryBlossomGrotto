3393a2dd020c4755dec6f5bdefb1db718a6be217
Dekunutter <jordan.griffin@outlook.ie>
2022-04-04 20:34:19 +0100

    Fix imports for modded block state properties

--- src/main/java/com/deku/cherryblossomgrotto/common/blocks/ModBlockStateProperties.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/blocks/ModBlockStateProperties.java
@@ -1,10 +1,10 @@
 package com.deku.cherryblossomgrotto.common.blocks;
 
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.EnumProperty;
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class ModBlockStateProperties {
     public static final BooleanProperty CHERRY_BLOSSOM_PETAL_COVERED = BooleanProperty.create("cherry_blossom_petal_covered");
