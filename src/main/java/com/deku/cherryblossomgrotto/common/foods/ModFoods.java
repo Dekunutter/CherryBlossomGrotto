3bf869b73cd69ff069504b3e81ab2f867804752e
Dekunutter <jordan.griffin@outlook.ie>
2022-04-09 04:32:35 +0100

    Update imports on rice crops and related food items

--- src/main/java/com/deku/cherryblossomgrotto/common/foods/ModFoods.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/foods/ModFoods.java
@@ -1,10 +1,10 @@
 package com.deku.cherryblossomgrotto.common.foods;
 
-import net.minecraft.item.Food;
+import net.minecraft.world.food.FoodProperties;
 
 public class ModFoods {
-    public static final Food RICE = new Food.Builder().nutrition(2).saturationMod(0.3F).build();
-    public static final Food ONIGIRI = new Food.Builder().nutrition(6).saturationMod(0.8F).build();
-    public static final Food KOI = new Food.Builder().nutrition(2).saturationMod(0.1F).build();
-    public static final Food COOKED_KOI = new Food.Builder().nutrition(6).saturationMod(0.8F).build();
+    public static final FoodProperties RICE = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build();
+    public static final FoodProperties ONIGIRI = new FoodProperties.Builder().nutrition(6).saturationMod(0.8F).build();
+    public static final FoodProperties KOI = new FoodProperties.Builder().nutrition(2).saturationMod(0.1F).build();
+    public static final FoodProperties COOKED_KOI = new FoodProperties.Builder().nutrition(6).saturationMod(0.8F).build();
 }
--- src/main/java/com/deku/cherryblossomgrotto/common/items/Rice.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/items/Rice.java
@@ -2,13 +2,13 @@ package com.deku.cherryblossomgrotto.common.items;
 
 import com.deku.cherryblossomgrotto.common.blocks.ModBlocks;
 import com.deku.cherryblossomgrotto.common.foods.ModFoods;
-import net.minecraft.item.BlockNamedItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 
-public class Rice extends BlockNamedItem {
+public class Rice extends ItemNameBlockItem {
     public Rice() {
-        super(ModBlocks.RICE_PADDY, new Item.Properties().tab(ItemGroup.TAB_FOOD).food(ModFoods.RICE));
+        super(ModBlocks.RICE_PADDY, new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(ModFoods.RICE));
         setRegistryName("rice");
     }
 }
