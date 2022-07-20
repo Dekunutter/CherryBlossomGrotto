217add59015fa79112bdf6a78e52fa430990d847
Dekunutter <jordan.griffin@outlook.ie>
2022-07-20 04:44:48 +0100

    Update mod version and remove unused import

--- src/main/java/com/deku/cherryblossomgrotto/Main.java
+++ src/main/java/com/deku/cherryblossomgrotto/Main.java
@@ -63,7 +63,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
