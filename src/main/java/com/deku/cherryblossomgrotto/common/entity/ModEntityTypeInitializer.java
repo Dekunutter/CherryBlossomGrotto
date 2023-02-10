6d613e756037e42e9dfb2e611699dbad35509d45
Dekunutter <jordan.griffin@outlook.ie>
2023-02-10 04:51:38 +0000

    Add missing file for entity type reinitializationb

--- src/main/java/com/deku/cherryblossomgrotto/common/entity/ModEntityTypeInitializer.java
+++ src/main/java/com/deku/cherryblossomgrotto/common/entity/ModEntityTypeInitializer.java
@@ -1,31 +1,68 @@
 package com.deku.cherryblossomgrotto.common.entity;
 
 import com.deku.cherryblossomgrotto.common.entity.animal.tanooki.Tanooki;
-import com.deku.cherryblossomgrotto.common.entity.monster.Coralfish;
 import com.deku.cherryblossomgrotto.common.entity.vehicle.ModBoatEntity;
 import com.deku.cherryblossomgrotto.common.entity.passive.fish.KoiEntity;
 import com.deku.cherryblossomgrotto.common.entity.projectile.KunaiEntity;
 import com.deku.cherryblossomgrotto.common.entity.projectile.ShurikenEntity;
 import com.deku.cherryblossomgrotto.common.entity.vehicle.ModChestBoatEntity;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
-// NOTE: This may seem redundant due to ModEntityData already existing as an object holder class but those objects are only initialized on registration
-//  Since there are cases where we need entity types BEFORE all objects are registered, we have an alternative storage used during initialization for reading them
-public class EntityTypeInitializer {
-    public static EntityType<Entity> BOAT_ENTITY_TYPE = EntityType.Builder.of(ModBoatEntity::new, MobCategory.MISC).setCustomClientFactory(ModBoatEntity::new).sized(1.375f, 0.5625f).clientTrackingRange(10).build("mod_boat_entity");
+import static com.deku.cherryblossomgrotto.Main.MOD_ID;
 
-    public static EntityType<Entity> CHEST_BOAT_ENTITY_TYPE = EntityType.Builder.of(ModChestBoatEntity::new, MobCategory.MISC).setCustomClientFactory(ModChestBoatEntity::new).sized(1.375f, 0.5625f).clientTrackingRange(10).build("mod_chest_boat_entity");
+public class ModEntityTypeInitializer {
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, MOD_ID);
 
-    public static EntityType<Entity> KUNAI_ENTITY_TYPE = EntityType.Builder.of(KunaiEntity::new, MobCategory.MISC).setCustomClientFactory(KunaiEntity::new).sized(0.5f, 0.5f).clientTrackingRange(4).updateInterval(20).build("kunai_entity");
+    public static final RegistryObject<EntityType<ModBoatEntity>> BOAT_ENTITY_TYPE = ENTITY_TYPES.register("mod_boat_entity", () ->
+        EntityType.Builder.<ModBoatEntity>of(ModBoatEntity::new, MobCategory.MISC)
+            .setCustomClientFactory(ModBoatEntity::new)
+            .sized(1.375f, 0.5625f)
+            .clientTrackingRange(10)
+            .build(new ResourceLocation(MOD_ID, "mod_boat_entity").toString())
+    );
 
-    public static EntityType<Entity> SHURIKEN_ENTITY_TYPE = EntityType.Builder.of(ShurikenEntity::new, MobCategory.MISC).setCustomClientFactory(ShurikenEntity::new).sized(0.5f, 0.5f).clientTrackingRange(4).updateInterval(20).build("shuriken_entity");
+    public static final RegistryObject<EntityType<ModChestBoatEntity>> CHEST_BOAT_ENTITY_TYPE = ENTITY_TYPES.register("mod_chest_boat_entity", () ->
+        EntityType.Builder.<ModChestBoatEntity>of(ModChestBoatEntity::new, MobCategory.MISC)
+            .setCustomClientFactory(ModChestBoatEntity::new)
+            .sized(1.375f, 0.5625f)
+            .clientTrackingRange(10)
+            .build(new ResourceLocation(MOD_ID, "mod_chest_boat_entity").toString())
+    );
 
-    public static EntityType<KoiEntity> KOI_ENTITY_TYPE = EntityType.Builder.of(KoiEntity::new, MobCategory.WATER_AMBIENT).sized(0.7f, 0.4f).clientTrackingRange(4).build("koi_entity");
+    public static final RegistryObject<EntityType<KunaiEntity>> KUNAI_ENTITY_TYPE = ENTITY_TYPES.register("kunai_entity", () ->
+        EntityType.Builder.<KunaiEntity>of(KunaiEntity::new, MobCategory.MISC)
+            .setCustomClientFactory(KunaiEntity::new)
+            .sized(0.5F, 0.5F)
+            .clientTrackingRange(4)
+            .updateInterval(20)
+            .build(new ResourceLocation(MOD_ID, "kunai_entity").toString())
+    );
 
-    public static EntityType<Coralfish> CORALFFISH_ENTITY_TYPE = EntityType.Builder.of(Coralfish::new, MobCategory.MONSTER).sized(1.0f, 1.0f).clientTrackingRange(8).build("coralfish");
+    public static final RegistryObject<EntityType<ShurikenEntity>> SHURIKEN_ENTITY_TYPE = ENTITY_TYPES.register("shuriken_entity", () ->
+        EntityType.Builder.<ShurikenEntity>of(ShurikenEntity::new, MobCategory.MISC)
+            .setCustomClientFactory(ShurikenEntity::new)
+            .sized(0.5F, 0.5F)
+            .clientTrackingRange(4)
+            .updateInterval(20)
+            .build(new ResourceLocation(MOD_ID, "shuriken_entity").toString())
+    );
 
-    public static EntityType<Tanooki> TANOOKI_ENTITY_TYPE = EntityType.Builder.of(Tanooki::new, MobCategory.CREATURE).sized(1.0f, 1.0f).clientTrackingRange(8).build("tanooki");
+    public static final RegistryObject<EntityType<KoiEntity>> KOI_ENTITY_TYPE = ENTITY_TYPES.register("koi_entity", () ->
+        EntityType.Builder.<KoiEntity>of(KoiEntity::new, MobCategory.WATER_AMBIENT)
+            .sized(0.7f, 0.4f)
+            .clientTrackingRange(4)
+            .build(new ResourceLocation(MOD_ID, "koi_entity").toString())
+    );
+
+    public static final RegistryObject<EntityType<Tanooki>> TANOOKI_ENTITY_TYPE = ENTITY_TYPES.register("tanooki", () ->
+        EntityType.Builder.<Tanooki>of(Tanooki::new, MobCategory.CREATURE)
+            .sized(1.0f, 1.0f)
+            .clientTrackingRange(8)
+            .build(new ResourceLocation(MOD_ID, "tanooki").toString())
+    );
 }
